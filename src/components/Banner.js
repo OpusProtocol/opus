50615f2afb1633c2c9636a538f212acd4df6331f
truongphitam <tamphitruong@gmail.com>
2023-06-08 00:29:54 +0700

    Add menu for mobile

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,5 +1,48 @@
+import { useEffect, useState } from 'react'
 import bg from '../assets/img/bg_banner.png'
+import $ from 'jquery'
 const Banner = () => {
+  const click = useState(0)
+  useEffect(() => {
+    const select = (el, all = false) => {
+      el = el.trim()
+      if (all) {
+        return [...document.querySelectorAll(el)]
+      } else {
+        return document.querySelector(el)
+      }
+    }
+  
+    /**
+     * Easy event listener function
+     */
+    const on = (type, el, listener, all = false) => {
+      let selectEl = select(el, all)
+      if (selectEl) {
+        if (all) {
+          selectEl.forEach(e => e.addEventListener(type, listener))
+        } else {
+          selectEl.addEventListener(type, listener)
+        }
+      }
+    }
+
+    on('click', '.mobile-nav-toggle', function(e) {
+      if($('#navbar').hasClass('navbar-mobile')){
+        $('#navbar').removeClass('navbar-mobile')
+        $('.mobile-nav-toggle').addClass('bi-list')
+        if($('.mobile-nav-toggle').hasClass('bi-x')){
+          $('.mobile-nav-toggle').removeClass('bi-x')
+        }
+      }else{
+        $('#navbar').addClass('navbar-mobile')
+        $('.mobile-nav-toggle').addClass('bi-x')
+        if($('.mobile-nav-toggle').hasClass('bi-list')){
+          $('.mobile-nav-toggle').removeClass('bi-list')
+        }
+      }
+    })
+  }, [click])
     return (
       <section id="banner" class="d-lg-flex align-items-center">
         <div class="container">
